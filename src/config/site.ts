b7c4bc53460ac13df797acc982341feb9a05f1c1
GraydonvG <graydonvg@gmail.com>
2025-05-19 11:39:35 +0200

    update production url

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
   siteUrl:
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "https://scrape-bot-dunw.vercel.app/",
+      : "https://scrape-bot-eight.vercel.app/",
   name: "ScrapeBot",
   description:
     "A platform to visually create and manage web scrapers without writing code. Simplifying data extraction with an intuitive drag-and-drop interface and a scheduling system for automating data collection. Additionally, it includes the option to integrate AI into the web scraping process if desired.",
